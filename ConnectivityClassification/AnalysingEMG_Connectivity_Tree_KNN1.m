4b250c6da38dfda6ac1e04eb305f503f9d8e5160
Alicia Falcon Caro <142226222+AliciaFalconCaro@users.noreply.github.com>
2024-06-07 13:00:45 +0100

    Add files via upload

---
+++ ConnectivityClassification/AnalysingEMG_Connectivity_Tree_KNN1.m
@@ -0,0 +1,152 @@
+%EMG data analysis and classification (https://uk.mathworks.com/help/signal/ug/classify-arm-motions-using-emg-signals-and-deep-learning.html)
+%Include arfit folder, covm, mvar and the connectivity processing function
+clear all;clc;
+fs = 5120;
+%create datastore type with all the data
+datasetFolder = fullfile("EMGData");
+
+SignalDatastore = signalDatastore(datasetFolder,IncludeSubFolders=true,SampleRate=fs); %sds1 
+p = endsWith(SignalDatastore.Files,"d.mat");
+EMGsignal = subset(SignalDatastore,p); %sdssig
+
+%include and associate labels
+LabelsDatastore = signalDatastore(datasetFolder,SignalVariableNames=["motion";"data_start";"data_end"],IncludeSubfolders=true); %sds2 
+p = endsWith(LabelsDatastore.Files,"i.mat");
+EMGLabel = subset(LabelsDatastore,p); %sdslbl 
+
+%define the region of interest for each motion based on the labels. We remove the first label value (-1) since it is asociated with rest time. The other labels are converted into an array
+
+labels = {};
+
+i = 1;
+while hasdata(EMGLabel)
+
+    label = read(EMGLabel);
+    
+    idx_start = label{2}(2:end)';
+    idx_end =label{3}(2:end)';
+    value = categorical(label{1}(2:end)',[0 1 2 3 4 6 7 8 9], ...
+          [ "Relax" "Idle" "Fist" "Flexion" "Extension" "PinchIndex" "PinchMiddle" "PinchRing" "PinchSmall"]);
+    ROI = [idx_start idx_end];
+
+    % In some cases, the number of label values and ROIs are not equal.
+    % To eliminate these inconsistencies, remove the extra label value or ROI limits. 
+%     if numel(value) < size(ROI,1)
+%         ROI(end,:) = [];
+%     elseif numel(value) > size(ROI,1)
+%         value(end) = [];
+%     end
+% 
+    LabelTable = table(ROI,value);
+    labels{i} = {LabelTable};
+
+    i = i+1;
+end
+
+
+% New datastore containing the modified label data and display the ROI table from the first observation.
+LabelDatastore = signalDatastore(labels); %lblDS
+LabelsTable = preview(LabelDatastore);
+LabelsTable{1}
+
+% Combine the signal and label data into one datastore.
+DataStore = combine(EMGsignal,LabelDatastore); %DS
+
+%Preview combine data of first observation (subject 1, session 1)
+combinedData = preview(DataStore)
+
+%% Create a signal mask and call plotsigroi to display the labeled motion regions for the first channel of the first signal
+% figure
+% msk = signalMask(combinedData{2});
+% plotsigroi(msk,combinedData{1}(:,1))
+% 
+% %represent the signal after filtering/dowsampling
+% 
+% % Filter and downsample signal data
+% sig=combinedData{1};
+% roiTable = combinedData{2};
+% 
+% % Remove first and last rest periods from signal
+% sig(roiTable.ROI(end,2):end,:) = [];
+% sig(1:roiTable.ROI(1,1),:) = [];
+% 
+% % Shift ROI limits to account for deleting start and end of signal
+% roiTable.ROI = roiTable.ROI-(roiTable.ROI(1,1)-1);
+% 
+% % Create signal mask
+% m = signalMask(roiTable);
+% L = length(sig);
+% 
+% 
+% % Obtain sequence of category labels and remove relax and idle motions
+% mask = catmask(m,L);
+% 
+% sigfilt = lowpass(sig,50,fs);
+% downsig = downsample(sigfilt,3);
+% 
+% % Downsample label data
+% downmask = downsample(mask,3);
+% 
+% idx = ~ismember(downmask,{'Relax', 'Idle'}); 
+% 
+% downmask = downmask(idx);
+% downsig = downsig(idx,:);
+% 
+% % Create new signal mask without relax and idle categories
+% m2 = signalMask(downmask);
+% 
+% m2.SpecifySelectedCategories = true;
+% m2.SelectedCategories = [3 4 5 6 7 8 9];
+% downmask = catmask(m2);
+% 
+% %Downsample frequency
+% f=fs/3;
+% fs_downsample=ceil(f);
+% 
+% targetLength = length(downsig); %no chuncks
+% % Get number of chunks
+% numChunks = 1;
+% 
+% % Truncate signal and mask to integer number of chunks
+% sig = downsig(1:numChunks*targetLength,:);
+% mask = downmask(1:numChunks*targetLength);
+% 
+% % Create a cell array containing signal chunks
+% 
+% lblOut = reshape(mask,targetLength,1)';
+% lblOut = num2cell(lblOut,2);
+% 
+% figure(2)
+% msk = signalMask(lblOut{1});
+% plotsigroi(msk,sig(:,1))
+% 
+
+%% preprocess data and apply connectivity for each segment of data (1s segment)
+trainDatastore = transform(DataStore,@ConnectivityProcessing_v2); %tDS
+transformedData = preview(trainDatastore)
+
+%after downsample, the fs is changed. Downsampled by factor of 3
+f=fs/3;
+fs_downsample=ceil(f);
+
+
+
+%% load data
+traindata = readall(trainDatastore,"UseParallel",true);
+
+
+%% Convert Training  data from cells of arrays and categorical data to table containing the predictors and observations and categorical containing features.
+%and call the classification learner to compute the classification using
+%several models.
+
+B=cell2mat(traindata(:,1)');
+ans=B';
+X= array2table(ans); % observations x predictors (channels) table
+
+Y=grp2idx(traindata{1,2});
+for i=2:size(traindata(:,2),1)
+    n1=grp2idx(traindata{i,2}); %n1 contains the features as non-catagorical data (numbers from 1 to 7 representing each category) for each cell array
+    Y=cat(1,Y,n1); %Y contains the array observations x 1, with the list of features as doubles. 
+end
+
+classificationLearner
---
+++ ConnectivityClassification/ConnectivityProcessing_v2.m
@@ -0,0 +1,128 @@
+function Tsds = ConnectivityProcessing_v2(inputDS)
+
+% uses slidding windows, overlap and average of frequency. The labels are
+% averaged for each slidding window.
+
+% Filter and downsample signal data
+sig = inputDS{1};
+roiTable = inputDS{2};
+fs=5120;
+
+% Remove first and last rest periods from signal
+sig(roiTable.ROI(end,2):end,:) = [];
+sig(1:roiTable.ROI(1,1),:) = [];
+
+% Shift ROI limits to account for deleting start and end of signal
+roiTable.ROI = roiTable.ROI-(roiTable.ROI(1,1)-1);
+
+% Create signal mask
+m = signalMask(roiTable);
+L = length(sig);
+
+
+% Obtain sequence of category labels and remove relax and idle motions
+mask = catmask(m,L);
+
+sigfilt = lowpass(sig,100,fs);
+downsig = downsample(sigfilt,3);
+
+% Downsample label data
+downmask = downsample(mask,3);
+
+idx = ~ismember(downmask,{'Relax', 'Idle'}); 
+
+downmask = downmask(idx);
+downsig = downsig(idx,:);
+
+% Create new signal mask without relax and idle categories
+m2 = signalMask(downmask);
+
+m2.SpecifySelectedCategories = true;
+m2.SelectedCategories = [3 4 5 6 7 8 9];
+downmask = catmask(m2);
+
+%Downsample frequency
+f=fs/3;
+fs_downsample=ceil(f);
+
+
+
+targetLength = floor(fs_downsample/2); %0.5s Window length
+overlap = fix(targetLength/4); % Overlap 25%
+start_point = 1;
+NumberChunks = 1;
+Mode1 = 10; % ARFIT(10), Multichannel Yule-Walker(1) ---> BioSig toolbox 
+
+y=downsig;
+CH = size(y,2); % Number of channels
+Length=size(y,1);
+Fmax = fs_downsample/2;
+nfft=fs_downsample; %number of frequency bins
+
+[w, Am, Cm, sbc, fpe, th] = arfit(y, 1, 40, 'sbc'); % ---> ARFIT toolbox
+[tmp,p_opt] = min(sbc); % Optimum order for the MVAR model
+
+
+% Create a cell array containing signal chunks
+sigOut = {};
+
+
+while( start_point + targetLength - 1 < Length )
+
+       seg = y(start_point:start_point+targetLength-1,:);
+
+       mask1(NumberChunks,:) = downmask(start_point:start_point+targetLength-1);
+       seg = seg.*repmat(hamming(length(seg)),1,CH);
+       [A_ST,RCF,C_ST] = mvar(seg, p_opt, Mode1); % ---> BioSig toolbox
+       [PDC_ST(:,:,:,NumberChunks), DTF_ST(:,:,:,NumberChunks)] = PDC_DTF_matrix(A_ST,p_opt,fs_downsample,Fmax,nfft);
+       start_point = start_point + (targetLength-overlap);
+       NumberChunks = NumberChunks + 1;
+end
+
+    DTF_meanFrequency= squeeze(mean(DTF_ST, 3));
+
+  %%reduce dimensionality from 2D matrix per frequency to 1D matrix per
+  %%segment
+    for n=1:size(DTF_meanFrequency,3) %identifies connectivity per segment
+        CountFrequenyPoints=1;
+        for j=1:size(DTF_meanFrequency,1) %identifies column of connectivity matrix
+            for k=1:size(DTF_meanFrequency,2) %identifies row of connectivity matrix
+                Reduced_DTF(CountFrequenyPoints,n)=DTF_meanFrequency(k,j,n);
+                CountFrequenyPoints=CountFrequenyPoints+1;
+            end
+        end
+    end
+
+    for i=1:(NumberChunks-1)
+        sigOut{i,1}= Reduced_DTF(:,i); %DTF per each chunk of signal (576x1) for each segment(CH*CH)
+
+        v(i,:) = countcats(mask1(i,:));
+
+        HighestCategory(i)=v(i,1);
+        Category(i) = 1;
+
+        for j=1:size(v,2)
+            if (v(i,j)>HighestCategory(i))
+                HighestCategory(i)=v(i,j);
+                Category(i) = j;
+            end
+        end
+
+%     lblOut{i,2} = Labels';
+
+    end
+
+% Create a cell array containing mask chunks
+Labels= categorical (Category, [1 2 3 4 5 6 7], {'Fist' 'Flexion' 'Extension' 'PinchIndex' 'PinchMiddle' 'PinchRing' 'PinchSmall'});
+
+
+lblOut = num2cell(Labels',2);
+    
+% Output a two-column cell array with all chunks
+Tsds = [sigOut,lblOut];
+end
+
+
+
+    
+
